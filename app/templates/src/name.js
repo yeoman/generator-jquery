ded295c82769bc7b7ac008221144ebe98c99e22d
Pascal Hartig <phartig@rdrei.net>
2013-04-25 14:45:32 +0200

    Adjust template js style to rest

--- app/templates/src/name.js
+++ app/templates/src/name.js
@@ -6,18 +6,18 @@
  * Licensed under the <%= props.license %> license.
  */
 
-(function($) {
+(function ($) {
 
   // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
+  $.fn.awesome = function () {
+    return this.each(function (i) {
       // Do something awesome to each selected element.
       $(this).html('awesome' + i);
     });
   };
 
   // Static method.
-  $.awesome = function(options) {
+  $.awesome = function (options) {
     // Override default options with passed-in options.
     options = $.extend({}, $.awesome.options, options);
     // Return something awesome.
@@ -30,7 +30,7 @@
   };
 
   // Custom selector.
-  $.expr[':'].awesome = function(elem) {
+  $.expr[':'].awesome = function (elem) {
     // Is this element awesome?
     return $(elem).text().indexOf('awesome') !== -1;
   };
