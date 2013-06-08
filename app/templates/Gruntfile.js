0d271c1293f84086da890b31dbdb3052e96f7e49
zakdoek <t_o_mvandamme@hotmail.com>
2013-06-08 11:39:01 +0200

    Add grunt server command to run tests in browser.

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -71,6 +71,14 @@ module.exports = function (grunt) {
         tasks: ['jshint:test', 'qunit']
       },
     },
+    connect: {
+      server: {
+        options: {
+          hostname: '*',
+          port: 9000
+        }
+      }
+    }
   });
 
   // These plugins provide necessary tasks.
@@ -80,7 +88,9 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
+  grunt.registerTask('server', ['connect', 'watch']);
 };
