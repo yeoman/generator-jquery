e0b8d07e63ad1101d0accdf22c3f32b042d3d73c
Sindre Sorhus <sindresorhus@gmail.com>
2013-10-26 00:47:24 +0200

    Add load-grunt-tasks
    https://github.com/sindresorhus/load-grunt-tasks

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,6 +1,8 @@
 'use strict';
 
 module.exports = function (grunt) {
+  // Load all grunt tasks
+  require('load-grunt-tasks')(grunt);
   // Show elapsed time at the end
   require('time-grunt')(grunt);
 
@@ -89,15 +91,6 @@ module.exports = function (grunt) {
       }
     }
   });
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
