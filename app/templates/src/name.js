6708e67447379def6f3224c9299b454af10edcf7
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2014-05-27 11:01:26 +0100

    Changed props.name to new camelized property.

--- app/templates/src/name.js
+++ app/templates/src/name.js
@@ -9,30 +9,30 @@
 (function ($) {
 
   // Collection method.
-  $.fn.<%= props.name %> = function () {
+  $.fn.<%= camelname %> = function () {
     return this.each(function (i) {
       // Do something to each selected element.
-      $(this).html('<%= props.name %>' + i);
+      $(this).html('<%= camelname %>' + i);
     });
   };
 
   // Static method.
-  $.<%= props.name %> = function (options) {
+  $.<%= camelname %> = function (options) {
     // Override default options with passed-in options.
-    options = $.extend({}, $.<%= props.name %>.options, options);
+    options = $.extend({}, $.<%= camelname %>.options, options);
     // Return the name of your plugin plus a punctuation character.
-    return '<%= props.name %>' + options.punctuation;
+    return '<%= camelname %>' + options.punctuation;
   };
 
   // Static method default options.
-  $.<%= props.name %>.options = {
+  $.<%= camelname %>.options = {
     punctuation: '.'
   };
 
   // Custom selector.
-  $.expr[':'].<%= props.name %> = function (elem) {
+  $.expr[':'].<%= camelname %> = function (elem) {
     // Does this element contain the name of your plugin?
-    return $(elem).text().indexOf('<%= props.name %>') !== -1;
+    return $(elem).text().indexOf('<%= camelname %>') !== -1;
   };
 
 }(jQuery));
